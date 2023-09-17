42f1b3c14d92eb5468df77133c9d3213ed48ebbd
Namal <hi@namal.dev>
2023-09-17 21:44:38 +0100

    Respond to PING

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
-# Uncomment this to pass the first stage
 import socket
+
+PORT = 6379
+PING_RESPONSE_CONTENT = "+PONG\r\n"
+
+# Response has to be encoded as binary
+PING_RESPONSE = PING_RESPONSE_CONTENT.encode("utf-8")
 
 
 def main():
-    print("Starting server")
+    print("Starting server on port {}".format(PORT))
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", PORT), reuse_port=True)
+
+    while True:
+        # wait for client
+        (connection, client_ip) = server_socket.accept()
+        print("Client connected on IP: {}".format(client_ip))
+
+        # Respond to PING
+        connection.send(PING_RESPONSE)
 
 
 if __name__ == "__main__":
