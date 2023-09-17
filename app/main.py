2b20cf3f65f7992c4d968af8dd7c495fd70f0a00
Namal <hi@namal.dev>
2023-09-17 22:00:49 +0100

    Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import socket
 
 PORT = 6379
 PING_RESPONSE_CONTENT = "+PONG\r\n"
+REQUEST_BUFFER_SIZE = 1024
 
 # Response has to be encoded as binary
 PING_RESPONSE = PING_RESPONSE_CONTENT.encode("utf-8")
@@ -12,13 +13,21 @@ def main():
 
     server_socket = socket.create_server(("localhost", PORT), reuse_port=True)
 
-    while True:
-        # wait for client
-        (connection, client_ip) = server_socket.accept()
-        print("Client connected on IP: {}".format(client_ip))
+    # Wait for client
+    (connection, client_ip) = server_socket.accept()
+    print("Client connected on IP: {}".format(client_ip))
 
-        # Respond to PING
-        connection.send(PING_RESPONSE)
+    while True:
+        # Wait for a request
+        request = connection.recv(REQUEST_BUFFER_SIZE)
+        while len(request) > 0:
+            print("Request: {}".format(request))
+
+            # Respond to PING
+            connection.send(PING_RESPONSE)
+
+            # Wait for another request
+            request = connection.recv(REQUEST_BUFFER_SIZE)
 
 
 if __name__ == "__main__":
